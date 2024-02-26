3c36881a137e2d47f40efae8f82c7ac80079ad99
MarkoTodorovic <markec026@yahoo.com>
2024-02-26 20:23:33 +0100

    Update global styles and theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          500: "#42BBFF",
+          800: "#0C3247",
+          900: "rgba(66, 187, 255, 0.1)",
+        },
+        black: {
+          600: "#2E3757",
+          700: "#1D2032",
+          800: "#131625",
+          900: "#2E3757",
+        },
+        white: {
+          100: "#FFFFFF",
+          300: "#ADB3CC",
+          500: "#55597D",
+        },
+        purple: {
+          500: "#9542FF",
+          900: "rgba(149, 66, 255, 0.1)",
+        },
+        green: {
+          400: "##68D1BF",
+          500: "#42FF77",
+          900: "rgba(66, 255, 119, 0.1)",
+        },
       },
     },
   },
