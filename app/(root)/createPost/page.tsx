4e0b77b36dfcbfc4159d99d64c0c3bdfd5e7e95b
MarkoTodorovic <markec026@yahoo.com>
2024-03-01 14:27:16 +0100

    Add RightSideBar component to CreatePost page

--- app/(root)/createPost/page.tsx
+++ app/(root)/createPost/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import LeftSideBar from "@/components/leftSideBar/page";
+import RightSideBar from "@/components/rightSideBar/page";
 import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 import Link from "next/link";
@@ -12,33 +13,7 @@ const CreatePost = () => {
       <LeftSideBar />
       <h1 className="h1-bold w-full mt-10 ">CreatePost</h1>
 
-      <div className="bg-black-800 w-1/3 px-7  pt-10">
-        <Link href="/createPost" className="flex gap-[6px] mb-12">
-          <Image
-            src="/assets/images/avatar01.png"
-            width={36}
-            height={36}
-            alt="avatar"
-          />
-
-          <div>
-            <p className="paragraph-3-medium !text-white-100">Nikky Eya</p>
-            <p className="paragraph-4-regular">nikky@jsmastery.pro</p>
-          </div>
-        </Link>
-        <div>
-          <h3 className="paragraph-3-bold !text-white-100 mb-4">Tags</h3>
-          <div className="flex flex-col space-y-4">
-            {Array.from({ length: 8 }).map((item) => (
-              <Link href="">
-                <Badge className="rounded paragraph-3-medium bg-black-700">
-                  Next
-                </Badge>
-              </Link>
-            ))}
-          </div>
-        </div>
-      </div>
+      <RightSideBar />
     </section>
   );
 };
